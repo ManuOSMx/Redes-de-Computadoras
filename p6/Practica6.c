f7e5b96d7804a6e9049ebb3ad9672196ea6672a7
Manuel4612 <manuel4612@hotmail.com>
2020-12-10 22:46:29 -0600

    Practica 6 ARP

---
+++ p6/Practica6.c
@@ -0,0 +1,229 @@
+#include <sys/socket.h>
+#include <linux/if_packet.h>
+#include <net/ethernet.h> /* the L2 protocols */
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <sys/ioctl.h>
+#include <net/if.h>
+#include <netinet/in.h>
+
+
+
+unsigned char MACorigen[6],MACdestino[6];
+unsigned char MACbroad[6]={0xff,0xff,0xff, 0xff,0xff,0xff};
+
+//solicitud de ARP
+unsigned char ethertype[2]={0x08, 0x06};
+unsigned char tramaEnv[1514], tramaRec[1514];
+unsigned char IPorigen[4];
+unsigned char IPdestino[4];
+unsigned char opcodeRes[2]={0x00,0x02};
+
+//COLORES
+
+void yellow(){
+	printf("\033[93m");//texto amarillo
+}
+
+void red() {
+	printf("\033[31m");//texto rojo
+}
+void blue() {
+	printf("\033[34m");//texto Azul
+}
+
+void MACColor() {
+	printf("\033[0m");
+	printf("\033[44m");//MAC DESTINO RESALTAR
+}
+
+void MACo() {
+	printf("\033[0m");
+	printf("\033[42m");//MAC ORIGEN VERDE
+}
+
+void IPo() {
+	printf("\033[0m");
+	printf("\033[46m");//IP ORIGEN AZUL CLARO
+}
+
+void tramaColor() {
+	printf("\033[0m");
+	printf("\033[45m");//TRAMA	
+}
+
+void resetColors() {
+	printf("\033[0m");//blanco
+	printf("\033[49m");//Quitar Subrayado
+}
+
+void imprimirTrama(unsigned char *paq, int len){
+    int i;
+   
+    for(i=0; i<len; i++){
+        if(i%16==0)
+        printf("\n\t");
+        printf("%.2x ", paq[i]);
+    }
+    resetColors();
+    printf("\n");
+}
+
+void estructuraTrama(unsigned char *trama){
+    char IP[50];
+    printf("\nInserta la direccion IP: ");
+    gets(IP);
+    inet_aton(IP,IPdestino);
+    unsigned char tipoh[2]={0x00,0x01};//ethernet interfaz de red
+    unsigned char tipoP[2]={0x08,0x00};//Protocolo que utiliza los servicios de ARP
+    unsigned char lonH[1]={6};//longitud de las direcciones de hardware
+    unsigned char longP[1]={4};//longitud de Direcciones ip
+    unsigned char codO[2]={0x00,0x01};//codigo de operacion
+   
+
+    //encabezado MAC
+    red();
+    memcpy(trama+0, MACbroad, 6);
+    memcpy(trama+6, MACorigen, 6);
+    memcpy(trama+12, ethertype,2);
+    //Mesaje de ARP
+    blue();
+    memcpy(trama+14,tipoh,2);
+    memcpy(trama+16,tipoP,2);
+    memcpy(trama+18,lonH,1);
+    memcpy(trama+19,longP,1);
+    memcpy(trama+20,codO,2);
+    memcpy(trama+22, MACorigen, 6);
+    memcpy(trama+28, IPorigen, 4);
+    memset(trama+32, 0x00, 6);
+    memcpy(trama+38,IPdestino, 4);
+}
+
+void enviarTrama( int ds, int index, unsigned char *trama){
+    
+    //llenar nuestra estrucutra sockaddr
+    int tam;
+    struct sockaddr_ll interfaz;
+    memset(&interfaz, 0x00, sizeof(interfaz));
+    interfaz.sll_family=AF_PACKET;
+    interfaz.sll_protocol=htons(ETH_P_ALL);
+    interfaz.sll_ifindex=index;
+    //se hace el envio
+    tam=sendto(ds, trama, 60, 0, (struct sockaddr *)&interfaz, sizeof(interfaz));
+
+    if(tam==-1){
+        perror("\nError al enviar");
+        exit(0);
+    }
+    else{
+        perror("\nExito al enviar");
+    }
+}
+
+void recibirTrama(int ds, unsigned char *trama){
+    //funcion para chat, recvfrom
+    unsigned char etherARP[2]={0x08,0x06}; 
+    while(1){
+        int tam;  
+        tam=recvfrom(ds, trama, 60, 0, NULL, 0);
+        if(tam==-1){
+            perror("\nError al recibir");
+            exit(0);
+        }
+        else{
+            
+            if(!(memcmp(trama+0,MACorigen,6) || memcmp(trama+12,etherARP,2)) && !(memcmp(trama+20,opcodeRes,2)|| memcmp(trama+28,IPdestino,4))){
+                tramaColor();
+                imprimirTrama(trama, tam);
+                memcpy(MACdestino,trama+22,6);
+                printf("\nMAC: ");
+                MACColor();
+                imprimirTrama(MACdestino, 6);
+              
+                break;
+            }
+        }
+    }  
+    
+}
+
+int obtenerDatos(int ds){
+    struct ifreq nic;
+    unsigned char nombre[20];
+    int i, index;
+    yellow();
+    printf("\n*****************************************\n");
+    resetColors();
+    printf("Inserta el nombre de la interfaz: ");
+    gets(nombre);
+
+    strcpy(nic.ifr_name, nombre);
+    if(ioctl(ds, SIOCGIFINDEX, &nic)==-1){
+        perror("\nError al obtener el index");
+    }
+    else{ 
+        index=nic.ifr_ifindex;
+        printf("El indice es %d", index);
+        if(ioctl(ds, SIOCGIFHWADDR, &nic)==-1){
+            perror("\nError al obtener la MAC");
+            exit(0);
+        }
+        else{
+            memcpy(MACorigen, nic.ifr_hwaddr.sa_data+0, 6);
+            printf("\nMAC Origen:\t");
+            MACo();
+            for(i=0; i<6; i++){
+                printf("%.2x:", MACorigen[i]);
+            }
+            resetColors();
+            if(ioctl(ds, SIOCGIFADDR, &nic)==-1){
+            perror("\nError al obtener la IP");
+            exit(0);
+              }
+           else{
+            memcpy(IPorigen, nic.ifr_addr.sa_data+2, 4);
+            printf("\nIP Origen:\t");
+            IPo();
+            		for(i=0; i<6; i++){
+               	 printf("%2d.", IPorigen[i]);
+            		}
+            	resetColors();
+        	}
+        yellow();
+        printf("\n\n*****************************************\n");
+        resetColors();
+        }
+        
+    }
+    return index;
+}
+
+/*>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>><>*/
+int main(){
+	
+	
+    int packet_socket, indice;
+    packet_socket = socket(AF_PACKET, SOCK_RAW, htons(ETH_P_ALL)); //abrimos nuestro socket
+
+    if(packet_socket==-1){
+        perror("Error al abrir el socket");
+        exit(0);
+    }
+    else{
+        perror("\nExito al abrir el socket");
+        indice=obtenerDatos(packet_socket);//se le manda el descriptor de socket
+        estructuraTrama(tramaEnv);
+        enviarTrama(packet_socket, indice, tramaEnv);
+        recibirTrama(packet_socket, tramaRec);
+    }
+
+    close(packet_socket); //cerramos nuestro socket
+	resetColors();
+	printf("\n");
+    return 0;
+}
+
+
